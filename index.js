24d3fdd891df888331a8e633fc30b97483d7d4cc
linarydholm <lina.rydholm@gmail.com>
2022-10-06 14:44:40 +0200

    Lina2

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ console.log("hello");
 
 let sofia = document.querySelector("#change");
 sofia.innerText = "Hej igen!";
+
 let viktoriasDiv = document.querySelector("#viktoria"); //pekar på min div
+
+let lina = document.querySelector("#linas-p");
+lina.innerText = "Ändrar texten på denna";
