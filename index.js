de5bd7811089a5cdcf6e51f92a7fe3161f31a80b
SofiaJe <sofia.jespersen@hotmail.com>
2022-10-06 14:41:00 +0200

    gav knappen funktion

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 console.log("hello");
 
 let sofia = document.querySelector("#change");
-sofia.innerText = "Hej igen!";
+let btn = document.querySelector("#changeText");
+btn.addEventListener("click", () => {
+    sofia.innerText = "Hej igen!";
+});
+
 let viktoriasDiv = document.querySelector("#viktoria"); //pekar på min div
